f2ca3a6cefdfd9b2c98a56f1ae178c048733cd14
haliltprak <haliltoprak4713@gmail.com>
2023-01-18 10:45:25 +0300

    Home component, Post component, Navbar component, User component yeniden olusturuldu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Switch, Route, Routes} from "react-router-dom";
+import Home from './components/Home/Home';
+import User from './components/User/User';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+          <Navbar></Navbar>
+          <Routes>
+              <Route exact path='/' element={<Home/>}></Route>
+              <Route exact path='/users/:userId' element={<User/>}></Route>
+          </Routes>
+        </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
